f3aedb1e0cfce19152f93ee547a401d9649db142
LEO <LEO@DESKTOP-MU8M68N>
2025-11-01 18:42:21 -0400

    Insertar y eliminar lista

--- src/proyecto1/Lista.java
+++ src/proyecto1/Lista.java
@@ -13,4 +13,27 @@ public class Lista {
     public Lista(){
         primero= null; 
     }
+    public void insertar(String usuario){
+        Nodo nuevo= new Nodo(usuario); 
+        if(this.primero==null){
+            this.primero= nuevo;
+        }else{
+            Nodo aux= this.primero;
+            while(aux.sig!=null){
+                aux=aux.sig; 
+            }
+            aux.sig= nuevo;
+        }
+    }
+        public void eliminar(String usuario){
+            if (this.primero!=null){
+                Nodo aux= this.primero;
+                while(aux.sig!=null && !aux.sig.usuario.equals(usuario)){
+                 aux=aux.sig;
+                }
+                if(aux.sig!=null){
+                    aux.sig= aux.sig.sig;
+                }             
+    }
+}
 }
